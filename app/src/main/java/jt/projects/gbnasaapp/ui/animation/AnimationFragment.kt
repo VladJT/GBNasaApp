77912233c3d7449c82d6b64640b22bcf8b35e82b
Богданков Владислав <onlyrus@hotmail.com>
2022-10-16 13:23:38 +0300

    add animations

--- app/src/main/java/jt/projects/gbnasaapp/ui/animation/AnimationFragment.kt
+++ app/src/main/java/jt/projects/gbnasaapp/ui/animation/AnimationFragment.kt
@@ -2,9 +2,13 @@ package jt.projects.gbnasaapp.ui.animation
 
 import android.graphics.Rect
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
@@ -15,6 +19,7 @@ class AnimationFragment : Fragment() {
     private var _binding: FragmentAnimationsBinding? = null
     private val binding get() = _binding!!
     private var textIsVisible = false
+    private var toRightAnimation = false
 
     companion object {
         fun newInstance() = AnimationFragment()
@@ -30,6 +35,16 @@ class AnimationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setButtonTransitionsListener()
+        setButtonPathListener()
+        createShuffles()
+        binding.recyclerViewExplode.adapter = Adapter()
+    }
+
+
+    // 1. ПРОСТАЯ АНИМАЦИЯ
+    private fun setButtonTransitionsListener() {
         binding.buttonTransitions.setOnClickListener {
             // Виды Transition
             //можно менять: они определяются вторым аргументом для функции beginDelayedTransition:
@@ -47,25 +62,57 @@ class AnimationFragment : Fragment() {
             textIsVisible = !textIsVisible
 
             //TransitionManager.beginDelayedTransition(binding.transitionsContainer, Slide(Gravity.END))//Аналог Fade. Элемент появляется, не меняя видимость, а выезжая из-за границы экрана
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer)
-            binding.textSlide.visibility = if (textIsVisible) View.VISIBLE else View.GONE
-            binding.textTransitionDefault.visibility =
+            TransitionManager.beginDelayedTransition(binding.containerTransitions)
+            binding.textTransition.visibility =
                 if (textIsVisible) View.VISIBLE else View.GONE
         }
-        binding.recyclerView.adapter = Adapter()
     }
 
-    /**
-     * Пустой адаптер, пустой холдер. Вся магия у нас происходит по клику на любой элемент списка в
-    методе Explode. Разберёмся, что в нём происходит. Для начала нам понадобится прямоугольник с
-    координатами по углам экрана. Внутри этого прямоугольника мы запустим нашу анимацию, в
-    процессе которой элементы улетят за границы прямоугольника, то есть экрана. Для этого мы
-    создадим класс Rect и передадим ему координаты при помощи метода View.getGlobalVisbleRect.
-    Затем мы создаём Transition типа Explode и вешаем на неё EpicenterCallback, который мы
-    упоминали выше. Он возвращает прямоугольник с границами нашего экрана. Далее устанавливаем
-    длительность анимации и вызываем уже знакомый нам метод beginDelayedTransition. Запустите
-    и посмотрите, что получилось. Эпицентром “взрыва” является тот элемент, на который вы нажимаете.
-     */
+    // 2. ДВИЖЕНИЕ ПО ТРАЕКТОРИИ
+    private fun setButtonPathListener() {
+        binding.buttonPath.setOnClickListener {
+            val changeBounds = ChangeBounds()
+            changeBounds.setPathMotion(ArcMotion())
+            changeBounds.duration = 500
+            TransitionManager.beginDelayedTransition(
+                binding.containerTransitions,
+                changeBounds
+            )
+            toRightAnimation = !toRightAnimation
+            val params = binding.buttonPath.layoutParams as FrameLayout.LayoutParams
+            params.gravity =
+                if (toRightAnimation) Gravity.END or Gravity.BOTTOM else Gravity.START or Gravity.TOP
+            binding.buttonPath.layoutParams = params
+        }
+    }
+
+    // 3. ПЕРЕМЕШИВАНИЕ ОБЪЕКТОВ
+    private fun createShuffles() {
+        val titles = mutableListOf(" [1] ", " [2] ", " [3] ", " [4] ", " [5] ")
+        createViews(binding.containerShuffle, titles)
+        binding.buttonShuffle.setOnClickListener {
+            TransitionManager.beginDelayedTransition(
+                binding.containerShuffle,
+                ChangeBounds()
+            )
+            titles.shuffle()
+            createViews(binding.containerShuffle, titles)
+        }
+    }
+
+    private fun createViews(layout: ViewGroup, titles: List<String>) {
+        layout.removeAllViews()
+        for (title in titles) {
+            val textView = TextView(requireContext())
+            textView.text = title
+            textView.gravity = Gravity.CENTER_HORIZONTAL
+            ViewCompat.setTransitionName(textView, title)
+            layout.addView(textView)
+        }
+    }
+
+
+    // 4. АНИМАЦИЯ ВЗРЫВА
     private fun explode(clickedView: View) {
         val viewRect = Rect()
         clickedView.getGlobalVisibleRect(viewRect)
@@ -84,17 +131,21 @@ class AnimationFragment : Fragment() {
                     transition.removeListener(this)
                 }
             })
-        TransitionManager.beginDelayedTransition(binding.recyclerView, set)
-
-        binding.recyclerView.adapter = null
+        TransitionManager.beginDelayedTransition(binding.recyclerViewExplode, set)
+        binding.recyclerViewExplode.adapter = null
     }
 
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
+    /**
+     * Пустой адаптер, пустой холдер. Вся магия у нас происходит по клику на любой элемент списка в
+    методе Explode. Разберёмся, что в нём происходит. Для начала нам понадобится прямоугольник с
+    координатами по углам экрана. Внутри этого прямоугольника мы запустим нашу анимацию, в
+    процессе которой элементы улетят за границы прямоугольника, то есть экрана. Для этого мы
+    создадим класс Rect и передадим ему координаты при помощи метода View.getGlobalVisbleRect.
+    Затем мы создаём Transition типа Explode и вешаем на неё EpicenterCallback, который мы
+    упоминали выше. Он возвращает прямоугольник с границами нашего экрана. Далее устанавливаем
+    длительность анимации и вызываем уже знакомый нам метод beginDelayedTransition. Запустите
+    и посмотрите, что получилось. Эпицентром “взрыва” является тот элемент, на который вы нажимаете.
+     */
     private inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             return ViewHolder(
@@ -118,5 +169,11 @@ class AnimationFragment : Fragment() {
     }
 
     private inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
 
